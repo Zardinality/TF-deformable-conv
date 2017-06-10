f1d6a1e2ac52cdf950938ab875962eed934492a2
Zardinality <asxaqz2@gmail.com>
2017-06-10 19:08:20 +0800

    change filename, and everything related

--- lib/g++_complie.sh
+++ lib/g++_complie.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -shared -o deform_conv.so deform_update.cc deform_conv.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_HOME/lib64 -D GOOGLE_CUDA=1 -Wfatal-errors -I $CUDA_HOME/include -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 -shared -o deform_conv.so deform_conv.cc deform_conv.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_HOME/lib64 -D GOOGLE_CUDA=1 -Wfatal-errors -I $CUDA_HOME/include -D_GLIBCXX_USE_CXX11_ABI=0
