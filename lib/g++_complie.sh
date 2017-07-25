2acd6bd6495534bf02c6e3b4897438445fd1973b
Zardinality <asxaqz2@gmail.com>
2017-07-25 13:17:38 +0800

    add cuda_config.h handle in g++_compile.sh

--- lib/g++_complie.sh
+++ lib/g++_complie.sh
@@ -1,1 +1,6 @@
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+echo $TF_INC
+if [ ! -f $TF_INC/tensorflow/stream_executor/cuda/cuda_config.h ]; then
+    cp ./cuda_config.h $TF_INC/tensorflow/stream_executor/cuda/
+fi
 g++ -std=c++11 -shared -o deform_conv.so deform_conv.cc deform_conv.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_HOME/lib64 -D GOOGLE_CUDA=1 -Wfatal-errors -I $CUDA_HOME/include -D_GLIBCXX_USE_CXX11_ABI=0
--- lib/nvcc_compile.sh
+++ lib/nvcc_compile.sh
@@ -1,1 +1,3 @@
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+echo $TF_INC
 nvcc -std=c++11 -c -o deform_conv.cu.o deform_conv.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -L /usr/local/cuda-8.0/lib64/ --expt-relaxed-constexpr
